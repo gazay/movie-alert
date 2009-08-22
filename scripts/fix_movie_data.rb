335285062885df4e77fb05063fe7d9b6fa5a6d2b
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 15:38:11 +0400

    Fix script to update DB ver 2

--- scripts/fix_movie_data.rb
+++ scripts/fix_movie_data.rb
@@ -12,14 +12,15 @@ i = 0
 Movie.all.each do |movie|
   begin
     data = ImdbMovie.new(movie.imdb_id)
+    data.get_data
+    i += 1
+    puts "#{i}/#{all} #{movie.imdb_id} #{data.title} #{data.year} #{data.release_date}"
+    movie.title = data.title
+    movie.year = data.year
+    movie.release_date = data.release_date
+    movie.save
   rescue
+    i -= 1
     retry
   end
-  data.get_data
-  i += 1
-  puts "#{i}/#{all} #{movie.imdb_id} #{data.title} #{data.year} #{data.release_date}"
-  movie.title = data.title
-  movie.year = data.year
-  movie.release_date = data.release_date
-  movie.save
 end
