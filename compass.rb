243c72601812aa73ddae030ebe2e8d311990da7c
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 04:09:25 +0400

    Add base content in SASS for Compass

---
+++ compass.rb
@@ -0,0 +1,8 @@
+require 'susy'
+
+project_type    = :stand_alone
+http_path       = '/'
+css_dir         = 'public/stylesheets'
+sass_dir        = 'stylesheets'
+images_dir      = 'public'
+javascripts_dir = 'public/javascript'
