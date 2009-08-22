a5765f68c5f54c55df0e3a9d42c3dfc452920518
Alex Gaziev <alex.gaziev@gmail.com>
2009-08-22 17:56:12 +0400

    Subscriptions collection calls subs

--- scripts/subscription.rb
+++ scripts/subscription.rb
@@ -4,14 +4,14 @@ module Subscription
   attr_accessor :movies_with_release_date
   
   def add_sub(targets, emails, twits)
-    Subscription.create(:targets => targets, :emails => emails, :twits => twits)
+    Subs.create(:targets => targets, :emails => emails, :twits => twits)
   end
   
   def add_email(targets, new_mail)
     if new_mail.match /^[a-zA-Z0-9]+
       ([_.-]?[a-zA-Z0-9]+)?@[a-zA-Z0-9]+([_-]?[a-zA-Z0-9]+)*([.]{1})
       [a-zA-Z0-9]+([.]?[a-zA-Z0-9]+)*$ /
-      s = Subscription.find(:targets => targets)
+      s = Subs.find(:targets => targets)
       if s
         s.emails << new_email
         s.save
@@ -23,7 +23,7 @@ module Subscription
   
   def add_twit(targets, new_twit)
     if new_twit.match /^[a-zA-Z0-9_]+$/
-      s = Subscription.find(:targets => targets)
+      s = Subs.find(:targets => targets)
       if s
         s.twits << new_twit
         s.save
