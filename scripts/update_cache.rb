a2d283ef04f146d640a5f406c52fcc06f436f120
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 20:57:46 +0400

    Add script to generate cache

--- scripts/update_cache.rb
+++ scripts/update_cache.rb
@@ -4,19 +4,15 @@ require File.join(File.dirname(__FILE__), '..', 'database')
 
 Cache.drop
 
-count = Movie.all.count
+count = Movies.find.count
 i = 0
-months = Movie.all.to_a.inject([]) { |all, movie|
+months = []
+Movies.find.each do |movie|
   i += 1
   puts "#{i}/#{count}"
-  
-  if movie.release_date
-    all + [movie.release_date[0..7]]
-  else
-    all
-  end
-}.uniq!
+  months << [movie['release_date'][0..6]] if movie['release_date']
+end
+months.uniq!.sort!
 
-p months
 
-#Cache.insert 'release_month' => month
+Cache.insert 'release_month' => months
