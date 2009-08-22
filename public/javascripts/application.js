8ffaaa30069dbb76c8a497798b589f946dfe4166
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 08:31:56 +0400

    Hightlight used filters

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,23 @@ jQuery(function($) {
     $('.default').focus(function() {
         var el = $(this)
         if (el.hasClass('default')) {
-            el.data('defaultValue', el.val()).removeClass('default').val('')
+            el.data('defaultValue', el.val()).removeClass('default').val('').
+                    parents('li').addClass('used')
         }
     }).blur(function() {
         var el = $(this)
         if ('' == el.val()) {
-            el.addClass('default').val(el.data('defaultValue'))
+            el.addClass('default').val(el.data('defaultValue')).
+                    parents('li').removeClass('used')
+        }
+    })
+    
+    $('#movie_genre').change(function() {
+        var el = $(this)
+        if (0 == el.attr('selectedIndex')) {
+            el.parents('li').removeClass('used')
+        } else {
+            el.parents('li').addClass('used')
         }
     })
 })
