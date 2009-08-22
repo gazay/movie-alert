aa4089dafd099dc8fb1ebef76318b97cabeaf241
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 15:27:13 +0400

    Add script to update DB

---
+++ scripts/change_movie_data.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require File.join(File.dirname(__FILE__), '..', 'database')
+
+gem 'porras-imdb'
+require 'imdb'
+
+all = Movie.all.count
+
+i = 0
+Movie.all.each do |movie|
+  data = ImdbMovie.new(movie.imdb_id)
+  data.get_data
+  i += 1
+  puts "#{i}/#{all} #{movie.imdb_id} #{data.title} #{data.year} #{data.release_date}"
+  movie.title = data.title
+  movie.year = data.year
+  movie.release_date = data.release_date
+  movie.save
+end
