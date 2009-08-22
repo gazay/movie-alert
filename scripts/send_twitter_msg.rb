5812cafa80d47b31755b5e6cb31f7078aa8114d9
Alex Gaziev <alex.gaziev@gmail.com>
2009-08-22 17:06:53 +0400

    Create module Twit

--- scripts/send_twitter_msg.rb
+++ scripts/send_twitter_msg.rb
@@ -2,15 +2,17 @@ require "rubygems"
 gem('twitter4r', '0.3.1')
 require 'twitter'
 
-attr_reader :twitter
+module Twit
+  attr_reader :twitter
 
-def connect
-  @twitter = Twitter::Client.from_config('files/twit_credentials.yml')
-end
+  def connect
+    @twitter = Twitter::Client.from_config('files/twit_credentials.yml')
+  end
 
-def send_message(message, user=nil, users=[])
-  twitter.message(:post, message, user) if user
-  users.each do |usr|
-    twitter.message(:post, message, usr) if usr
+  def send_message(message, user=nil, users=[])
+    twitter.message(:post, message, user) if user
+    users.each do |usr|
+      twitter.message(:post, message, usr) if usr
+    end
   end
 end
