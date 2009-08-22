2795e85e6ea00d5e391a4941fb69167c57354f43
brainopia <ravwar@gmail.com>
2009-08-22 13:49:15 +0400

    Fix bug with indexes

--- database.rb
+++ database.rb
@@ -6,7 +6,7 @@ class Movie < MongoRecord::Base
   collection_name :movies
 
   fields :year, :title, :poster, :plot, :genres, :director, :cast_members
-  index :title, :director, :cast_members, :genres
+  indexes :title, :director, :cast_members, :genres
 
   def to_s
     "ID: #{id}, title: #{title}, year: #{year}"
