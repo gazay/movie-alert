0eaeced7c24064aab8ceff29f1659e6e1f2bebe8
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-22 18:40:39 +0400

    Show progress in cache script

--- scripts/update_cache.rb
+++ scripts/update_cache.rb
@@ -4,9 +4,14 @@ require File.join(File.dirname(__FILE__), '..', 'database')
 
 Cache.drop
 
+count = Movie.all.count
+i = 0
 months = Movie.all.to_a.inject([]) { |all, movie|
+  i += 1
+  puts "#{i}/#{count}"
+  
   if movie.release_date
-    all << movie.release_date[0..7]
+    all + [movie.release_date[0..7]]
   else
     all
   end
