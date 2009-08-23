07acced69310004689f8442ee65588e06cced8cd
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-08-23 23:52:48 +0400

    Add new caches to update_cache

--- scripts/update_cache.rb
+++ scripts/update_cache.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'set'
 require File.join(File.dirname(__FILE__), '..', 'database')
 
 Cache.clear
@@ -7,6 +8,11 @@ Cache.clear
 count = Movies.find.count
 i = 0
 dates = []
+
+genres = Set.new
+actors = Set.new
+directors = Set.new
+
 last_year = nil
 Movies.find.each do |movie|
   i += 1
@@ -15,6 +21,10 @@ Movies.find.each do |movie|
   if movie['release_date']
     dates << [movie['release_date'][0..3], movie['release_date'][5..6]]
   end
+  
+  genres.merge movie['genres']
+  actors.merge movie['actors']
+  directors << movie['director']
 end
 dates.uniq!.sort! { |a, b| a[0] + a[1] <=> b[0] + b[1] }
 
@@ -24,4 +34,9 @@ dates.each do |year, month|
   months[year] << month
 end
 
-Cache.insert :name => 'release_months', :value => months
+p genres.to_a
+
+#Cache.insert :name => 'release_months', :value => months
+#Cache.insert :name => 'directors', :value => directors.to_a
+#Cache.insert :name => 'actors', :value => actors.to_a
+#Cache.insert :name => 'genres', :value => genres.to_a
