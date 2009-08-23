71b23ed9cc315c97e8021e985f14f502e5d560a2
brainopia <ravwar@gmail.com>
2009-08-23 20:45:07 +0400

    Update database to work with new db

--- database.rb
+++ database.rb
@@ -7,9 +7,6 @@ DB = Mongo::Connection.new('97.107.138.149').db DB_NAME
 Subs = DB.collection 'subs'
 Cache = DB.collection 'cache'
 Movies = DB.collection 'movies'
-Genres = DB.collection 'genres'
-Actors = DB.collection 'actors'
-Directors = DB.collection 'directors'
 
 Movies.create_index 'genres'
 Movies.create_index 'actors'
