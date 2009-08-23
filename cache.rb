ca822381cf652036b694d29c26c1db7dd47974ca
brainopia <ravwar@gmail.com>
2009-08-23 21:22:19 +0400

    Genres are cached

--- cache.rb
+++ cache.rb
@@ -2,5 +2,5 @@ require "yaml"
 
 Actors = YAML.load_file('cache/actors.yml').join("\n")
 Directors = YAML.load_file('cache/directors.yml').join("\n")
-Genres = YAML.load_file('cache/genres.yml').join("\n")
+Genres = YAML.load_file('cache/genres.yml')
 
