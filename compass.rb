d05a138f9c7876ba5a1fd5be69336c1fb2ad37b9
brainopia <ravwar@gmail.com>
2009-09-11 08:27:32 +0400

    Remove compass config from top-layer

--- compass.rb
+++
@@ -1,8 +0,0 @@
-require 'susy'
-
-project_type    = :stand_alone
-http_path       = '/'
-css_dir         = 'public/stylesheets'
-sass_dir        = 'stylesheets'
-images_dir      = 'public'
-javascripts_dir = 'public/javascripts'
