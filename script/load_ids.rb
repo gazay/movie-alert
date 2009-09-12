e3bb03ab7eca62b57eb4ce9c7c821d8f76713889
brainopia <ravwar@gmail.com>
2009-09-12 17:56:37 +0400

    Rename scripts folder as script

---
+++ script/load_ids.rb
@@ -0,0 +1,23 @@
+require "open-uri"
+
+years = ['2009','2010','2011','2012']
+
+years.each do |year|
+  File.open("/Users/alexgaziev/ids#{year}.list", 'w+') do |file|
+    site = open("http://www.imdb.com/List?year=#{year}&&tvm=off&&tv=off&&ep=off&&vid=off", 'User-Agent' => 
+    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 Ubuntu/9.04 (jaunty) Shiretoko/3.5.2')
+    rd = site.read
+    all = rd.scan(/Here are the (\d+) matching titles/).first.first.to_i
+    puts all
+    
+    skip = 0
+    while all >= skip
+      ids = rd.scan(/\/title\/tt(\d+)\/">[^<]*<\/A><SMALL><\/SMALL>/)
+      file.puts(ids.map{ |i| i.first }.join("\n"))
+      skip += 200
+      puts year + ', skip=' + skip.to_s
+      rd = open("http://www.imdb.com/List?tvm=off&&tv=off&&ep=off&&vid=off&&year=#{year}&&skip=#{skip}", 'User-Agent' => 
+        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 Ubuntu/9.04 (jaunty) Shiretoko/3.5.2').read
+    end
+  end
+end
---
+++ script/notifications.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'config/database'
+require 'lib/send'
+
+def find_subs(day, movies)
+  subs = Subs.find().to_a
+  
+  subs.each do |sub|
+    movies.each do |movie|
+      can_send = nil
+      sub['targets'].each do |key, value|
+        puts key.inspect + ' - ' + value.inspect
+        if key == 'director'
+          can_send = movie[key] == value
+        else
+          can_send = movie[key + 's'].include? value
+        end
+      end
+      if can_send && sub['twit']
+        Send.twit :to_user => sub['twit'],
+                  :message => day +
+                    ' release date of ' + movie['title'] + 
+                    '. Link: http://www.imdb.com/title/tt' + 
+                    movie['imdb_id'] + '/'
+        puts 'Send to @' + sub['twit']
+      elsif can_send && sub['email']
+        Send.mail :to => sub['email'],
+                  :subject => day +
+                    ' release date of ' + 
+                    movie['title'] + '.',
+                  :body => 'Link to film: ' +
+                    'http://www.imdb.com/title/tt' + 
+                    movie['imdb_id'] + '/'
+      end                          
+    end
+  end 
+end
+
+find_subs('Tomorow', Movies.find(:release_date => (Date.today + 1).to_s).to_a)
+find_subs('Next week', Movies.find(:release_date => (Date.today + 7).to_s).to_a)
