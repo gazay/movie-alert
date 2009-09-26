8f84c3125acfe3b99921c2a15ff35a6339a849d2
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-09-26 11:45:35 +0400

    Clean up load_ids script

--- script/load_ids.rb
+++ script/load_ids.rb
@@ -1,23 +1,30 @@
+#!/usr/bin/env ruby
+
 require "open-uri"
 
-years = ['2009','2010','2011','2012']
+YEARS = ['2009', '2010', '2011', '2012']
+AGENT = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 ' +
+        'Ubuntu/9.04 (jaunty) Shiretoko/3.5.2'
+QUERY = '&&tvm=off&&tv=off&&ep=off&&vid=off'
 
-years.each do |year|
-  File.open("/Users/alexgaziev/ids#{year}.list", 'w+') do |file|
-    site = open("http://www.imdb.com/List?year=#{year}&&tvm=off&&tv=off&&ep=off&&vid=off", 'User-Agent' => 
-    'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 Ubuntu/9.04 (jaunty) Shiretoko/3.5.2')
-    rd = site.read
-    all = rd.scan(/Here are the (\d+) matching titles/).first.first.to_i
+YEARS.reverse.each do |year|
+  File.open("ids#{year}.list", 'w+') do |file|
+    page = open("http://www.imdb.com/List?year=#{year}#{QUERY}",
+                'User-Agent' => AGENT).read
+    
+    all = page.scan(/Here are the (\d+) matching titles/).first.first.to_i
     puts all
     
     skip = 0
     while all >= skip
-      ids = rd.scan(/\/title\/tt(\d+)\/">[^<]*<\/A><SMALL><\/SMALL>/)
-      file.puts(ids.map{ |i| i.first }.join("\n"))
-      skip += 200
       puts year + ', skip=' + skip.to_s
-      rd = open("http://www.imdb.com/List?tvm=off&&tv=off&&ep=off&&vid=off&&year=#{year}&&skip=#{skip}", 'User-Agent' => 
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 Ubuntu/9.04 (jaunty) Shiretoko/3.5.2').read
+      
+      ids = page.scan(/\/title\/tt(\d+)\/">[^<]*<\/A><SMALL><\/SMALL>/)
+      file.puts(ids.map{ |i| i.first }.join("\n"))
+      
+      skip += 200
+      page = open("http://www.imdb.com/List?year=#{year}&&skip=#{skip}#{QUERY}",
+                  'User-Agent' => AGENT).read
     end
   end
 end
