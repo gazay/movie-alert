c32d0706af4ac93685432961810058874c2ebde5
Andrey A.I. Sitnik <andrey@sitnik.ru>
2009-09-26 13:54:18 +0400

    Rewrite paginator in load_ids

--- script/load_ids.rb
+++ script/load_ids.rb
@@ -2,7 +2,7 @@
 
 require "open-uri"
 
-YEARS = ['2009', '2010', '2011', '2012', '2013']
+YEARS = [2009, 2010, 2011, 2012, 2013]
 AGENT = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.2) Gecko/20090803 ' +
         'Ubuntu/9.04 (jaunty) Shiretoko/3.5.2'
 QUERY = '&&tvm=off&&tv=off&&ep=off&&vid=off'
@@ -13,18 +13,14 @@ YEARS.reverse.each do |year|
                 'User-Agent' => AGENT).read
     
     all = page.scan(/Here are the (\d+) matching titles/).first.first.to_i
-    puts all
+    puts "#{year} #{all}"
     
-    skip = 0
-    while all >= skip
-      puts year + ', skip=' + skip.to_s
-      
-      ids = page.scan(/\/title\/tt(\d+)\/">[^<]*<\/A><SMALL><\/SMALL>/)
-      file.puts(ids.map{ |i| i.first }.join("\n"))
-      
-      skip += 200
+    (0..all).step(200) do |skip|
+      puts "#{year} skip #{skip}"
       page = open("http://www.imdb.com/List?year=#{year}&&skip=#{skip}#{QUERY}",
                   'User-Agent' => AGENT).read
+      ids = page.scan(/\/title\/tt(\d+)\/">[^<]*<\/A><SMALL><\/SMALL>/)
+      file.puts(ids.map{ |i| i.first }.join("\n"))
     end
   end
 end
